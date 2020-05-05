d64411873e44201e203dbffde2e55405afc6ffdf
dmelwani99 <37684154+dmelwani99@users.noreply.github.com>
2020-05-05 12:17:27 -0400

    Changed os.walk to glob Changed to search for only .niigz, json, and gii files Began parsing file names

--- source/main.py
+++ source/main.py
@@ -2,35 +2,56 @@ import glob, os
 import numpy as np
 import pandas as pd
 
+def addToDataFrame(file, dataframe):
+        return dataframe.append({'file': os.path.basename(file), 'path' : file.replace(projectRoot, "")}, ignore_index=True)
+
 # user editable vars
 projectRoot = "/Users/deepak/Documents/PythonFiles/data/empty_proj"
 outputFileName = "scanResults"
 outputDirectory = "/Users/deepak/Documents/PythonFiles/data/outputFiles"
 
 # strutcture data used to start DataFrame w/ a single blank data row
-data = [('', '', '', '', '')]
+data = [('', '', '', '', '', '', '', '', '')]
 
 # create a DataFrame object w/ column labels
-dfObj = pd.DataFrame(data, columns=['file', 'path', 'parent', 'child', 'otherFiles'])
+dfObj = pd.DataFrame(data, columns=['file', 'path', 'parent', 'child', 'sub', 'ses', 'task', 'run', 'otherFiles'], index=['file'])
 
 print('Beginning search in ' + projectRoot)
 
-# go through each file in every directory/subdir in the project
-for path, subdirs, files in os.walk(projectRoot):
-    for name in files:
-        # add each file + path to the dataframe
-        dfObj = dfObj.append({'file': name, 'path' : os.path.join(path, name).replace(projectRoot, "")}, ignore_index=True)
+#print('Processing .nii.gz Files now')
 
-    print('Added ' + str(len(files)) + ' first-level files from ' + path)
+#niigzFiles = glob.glob(projectRoot + '/**/*.nii.gz', recursive=True)
+#for name in niigzFiles:
+#    dfObj = addToDataFrame(name, dfObj)
 
-    # debug break to speed up testing by cutting run time down
-    if len(files) == 1692:
-        break
+#print('Processing .json Files now')
+#
+#jsonFiles = glob.glob(projectRoot + '/**/*.json', recursive=True)
+#for name in jsonFiles:
+#    dfObj = addToDataFrame(name, dfObj)
 
+print('Processing .gii Files now')
+
+giiFiles = glob.glob(projectRoot + '/**/*.gii', recursive=True)
+for name in giiFiles:
+    dfObj = addToDataFrame(name, dfObj)
+
+dfObj.drop_duplicates(inplace=True)
 # iterate through DataFrame rows
 for index, row in dfObj.iterrows():
-    # do some logic to assign parents, children, and more
-    print(row['file'], row['path']) # for now just printing rows
+
+    fileName = row['file']
+    #split file name into labels separated by '_'
+    labels = fileName.split('_')
+
+    for label in labels:
+        if '-' in label:
+            info = label.split('-')
+            if info[0] == 'sub' or info[0] == 'ses' or info[0] == 'task' or info[0] == 'run':
+                dfObj.at[fileName, info[0]] = info[1]
+            else:
+                existingOtherFiles = dfObj.at[fileName, 'otherFiles']
+                dfObj.at[fileName, 'otherFiles'] = (str(existingOtherFiles) + '("label": "{}", "value": "{}")'.format(info[0], info[1]))
 
 # remove the first blank row used for structure
 dfObj = dfObj.drop(dfObj.index[0])
@@ -38,4 +59,4 @@ dfObj = dfObj.drop(dfObj.index[0])
 # export as CSV file
 dfObj.to_csv(outputDirectory + outputFileName + '.csv', index = False)
 
-print('Scan Complete. Exported to ' + projectRoot + 'FileName.csv')
+print('Scan Complete. Exported to ' + projectRoot + '/' + outputFileName)
